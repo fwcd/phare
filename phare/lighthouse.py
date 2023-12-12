0da4e5cae0c2fc4a4ebff3a3daf0b00f48bfc1a5
fwcd <fwcdmail@gmail.com>
2023-12-12 19:00:43 +0000

    Define dictionary conversions

--- phare/lighthouse.py
+++ phare/lighthouse.py
@@ -1,9 +1,17 @@
+from typing import TypeVar
 from websocket import create_connection # type: ignore
 
 from phare.auth import Auth
 from phare.constants import LIGHTHOUSE_URL
+from phare.convert import ToDict
+from phare.protocol import ClientMessage
+
+T = TypeVar("T")
 
 class Lighthouse:
     def __init__(self, auth: Auth, url: str = LIGHTHOUSE_URL):
         self.auth = auth
         self.websocket = create_connection(url)
+    
+    def send_message(self, message: ClientMessage[ToDict]):
+        pass
--- phare/protocol.py
+++ phare/protocol.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
-from typing import Generic, Optional, TypeVar
+from typing import Any, Generic, Optional, Self, TypeVar
 
 from phare.auth import Auth
+from phare.convert import FromDict, ToDict
 
-T = TypeVar("T")
+T = TypeVar("T", bound=ToDict)
+U = TypeVar("U", bound=FromDict)
 
 @dataclass
-class ClientMessage(Generic[T]):
+class ClientMessage(Generic[T], ToDict):
     request_id: int
     verb: str
     path: list[str]
@@ -14,17 +16,46 @@ class ClientMessage(Generic[T]):
     auth: Auth
     payload: T
 
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            'REID': self.request_id,
+            'VERB': self.verb,
+            'PATH': self.path,
+            'META': self.meta,
+            'AUTH': self.auth,
+            'PAYL': self.payload.to_dict(),
+        }
+
 @dataclass
-class ServerMessage(Generic[T]):
+class ServerMessage(Generic[U]):
     code: int
     request_id: Optional[int]
     warnings: list[str]
     response: Optional[str]
-    payload: T
+    payload: U
+
+    @classmethod
+    def from_dict(cls, raw: dict[str, Any], payload_type: type[U]) -> Self:
+        return ServerMessage(
+            code=raw['RNUM'],
+            request_id=raw.get('REID'),
+            warnings=raw.get('WARNINGS', []),
+            response=raw.get('RESPONSE'),
+            payload=payload_type.from_dict(raw['PAYL']),
+        )
 
 @dataclass
-class InputEvent:
+class InputEvent(FromDict):
     source: int
     key: Optional[int]
     button: Optional[int]
     is_down: bool
+
+    @classmethod
+    def from_dict(cls, raw: dict[str, Any]) -> Self:
+        return InputEvent(
+            source=raw['src'],
+            key=raw.get('key'),
+            button=raw.get('btn'),
+            is_down=raw['dwn'],
+        )
