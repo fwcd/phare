fd26227165370961b3b8e433dbd5d9d77e231ec7
fwcd <fwcdmail@gmail.com>
2023-12-12 19:13:52 +0000

    Use asynchronous websocket client

--- examples/fill_color.py
+++ examples/fill_color.py
@@ -1,13 +1,16 @@
+import asyncio
 import os
 
 from phare.auth import Auth
 from phare.lighthouse import Lighthouse
 from phare.constants import LIGHTHOUSE_URL
 
-user = os.environ['LIGHTHOUSE_USER']
-token = os.environ['LIGHTHOUSE_TOKEN']
-url = os.environ.get('LIGHTHOUSE_URL', LIGHTHOUSE_URL)
+async def main():
+    user = os.environ['LIGHTHOUSE_USER']
+    token = os.environ['LIGHTHOUSE_TOKEN']
+    url = os.environ.get('LIGHTHOUSE_URL', LIGHTHOUSE_URL)
 
-lh = Lighthouse(Auth(user, token), url)
+    with await Lighthouse.connect(Auth(user, token), url) as lh:
+        pass
 
-# TODO
+asyncio.run(main())
--- phare/lighthouse.py
+++ phare/lighthouse.py
@@ -1,17 +1,34 @@
-from typing import TypeVar
-from websocket import create_connection # type: ignore
+from typing import Any, Self, TypeVar
 
 from phare.auth import Auth
 from phare.constants import LIGHTHOUSE_URL
 from phare.convert import ToDict
 from phare.protocol import ClientMessage
 
+import msgpack # type: ignore
+import websockets
+
 T = TypeVar("T")
 
 class Lighthouse:
-    def __init__(self, auth: Auth, url: str = LIGHTHOUSE_URL):
+    def __init__(self, auth: Auth, websocket: websockets.WebSocketClientProtocol):
         self.auth = auth
-        self.websocket = create_connection(url)
+        self.websocket = websocket
     
-    def send_message(self, message: ClientMessage[ToDict]):
-        pass
+    @classmethod
+    async def connect(cls, auth: Auth, url: str = LIGHTHOUSE_URL) -> Self:
+        websocket = await websockets.connect(url)
+        return Lighthouse(
+            auth=auth,
+            websocket=websocket,
+        )
+
+    def __enter__(self):
+        return self
+    
+    async def __exit__(self, _exc_type: Any, _exc_value: Any, _exc_traceback: Any):
+        await self.websocket.close()
+
+    async def send_message(self, message: ClientMessage[ToDict]):
+        binary: bytes = msgpack.packb(message) # type: ignore
+        await self.websocket.send(binary)
